0dd7b386690f585aebe32d3e8f467482f470832d
JangGyuBeom <wkdrbqja@dreamwiz.com>
2016-11-18 22:05:23 +0900

    레시피 결과창 1차

--- Recipe/src/RecipeResult.java
+++ Recipe/src/RecipeResult.java
@@ -4,18 +4,91 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import java.awt.Font;
+import java.awt.Graphics;
+
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.Icon;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JList;
+import javax.swing.JTextArea;
 
 public class RecipeResult extends JFrame {
 
 	private JPanel contentPane;
-
-	public RecipeResult() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		setContentPane(contentPane);
+	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					RecipeResult frame = new RecipeResult();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
 	}
 
+	public RecipeResult() {
+		RecipeImage image = new RecipeImage();
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 819, 477);
+		contentPane = new JPanel(){
+ 			public void paintComponent(Graphics g){
+ 				g.drawImage(image.background_ora.getImage(),0,0,null);
+ 				setOpaque(false);
+ 				super.paintComponent(g);
+ 			}
+ 		};
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JScrollPane scrollPane = new JScrollPane();
+		scrollPane.setBounds(30, 70, 217, 297);
+		contentPane.add(scrollPane);
+		
+		JList list = new JList();
+		scrollPane.setViewportView(list);
+		
+		JScrollPane scrollPane_1 = new JScrollPane();
+		scrollPane_1.setBounds(270, 70, 500, 297);
+		contentPane.add(scrollPane_1);
+		
+		JTextArea textArea = new JTextArea();
+		scrollPane_1.setViewportView(textArea);
+		
+		JLabel label = new JLabel("\uC694\uB9AC");
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		label.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 28));
+		label.setBounds(30, 10, 217, 50);
+		contentPane.add(label);
+		
+		JLabel label_1 = new JLabel("\uB808\uC2DC\uD53C");
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		label_1.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 28));
+		label_1.setBounds(270, 10, 500, 50);
+		contentPane.add(label_1);
+		
+		JButton button = new JButton(image.Back);
+		button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				dispose();
+			}
+		});
+		button.setFocusPainted(false);
+		button.setContentAreaFilled(false);
+		button.setBorderPainted(false);
+		button.setBounds(12, 388, 50, 41);
+		contentPane.add(button);
+		
+		JLabel label_2 = new JLabel((Icon) null);
+		label_2.setBounds(12, 388, 50, 41);
+		contentPane.add(label_2);
+	}
 }
