947447a32a5b669184479d4e68386b8fab007651
JuYoungDo <sp242685@naver.com>
2016-11-27 20:05:18 +0900

    배달검색창 Exception

--- Recipe/btn/DeliveryButtonHandler.java
+++ Recipe/btn/DeliveryButtonHandler.java
@@ -8,186 +8,156 @@ public class DeliveryButtonHandler extends Price {
 	String sql;
 	Vector<?> vec;
 	int price;
-	
+
 	public void chinahandler() { // 중식
-		try {
-			
-			price = getPrice();
-			
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='중식'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+
+		price = getPrice();
+
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='중식'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
 
 	}
 
 	public void chickenhandler() { // 치킨
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='치킨'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='치킨'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
 
 	}
 
 	public void japanfoodhandler() { // 일식
-		try {
-			price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='일식'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		price = getPrice();
+
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='일식'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
+
 	}
 
 	public void koreanfoodhandler() { // 한식
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='한식/분식'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='한식/분식'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
 
 	}
 
 	public void westernfoodhandler() { // 양식
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='양식'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='양식'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
 
 	}
 
 	public void figsfeethandler() { // 족발
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='족발/보쌈'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='족발/보쌈'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
 
 	}
 
 	public void steamhandler() { // 찜
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='찜/탕'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='찜/탕'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
+
 	}
 
 	public void pizzahandler() { // 피자
 
-		try {
-			price = getPrice();
+		price = getPrice();
 
-			sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='피자'";
-			vec = cc.makeD_SQL(sql);
-			EventQueue.invokeLater(new Runnable() {
-				public void run() {
-					try {
-						DeliveryResult frame = new DeliveryResult(vec,price);
-						frame.setVisible(true);
-					} catch (Exception e) {
-						e.printStackTrace();
-					}
+		sql = "select distinct d_name from delivery where d_price<=" + price + " and d_group='피자'";
+		vec = cc.makeD_SQL(sql);
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					DeliveryResult frame = new DeliveryResult(vec, price);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
 				}
-			});
-		} catch (java.lang.NumberFormatException e) {
-			JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
-		}
+			}
+		});
+
 	}
 }
--- Recipe/src/RecipeDelivery.java
+++ Recipe/src/RecipeDelivery.java
@@ -7,6 +7,7 @@ import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.SwingConstants;
@@ -15,7 +16,6 @@ import javax.swing.border.EmptyBorder;
 public class RecipeDelivery extends JFrame {
 
 	private JPanel contentPane;
-	
 
 	public RecipeDelivery() {
 		Price p = new Price();
@@ -23,10 +23,10 @@ public class RecipeDelivery extends JFrame {
 		DeliveryButtonHandler delivery = new DeliveryButtonHandler();
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 786, 802);
-		
-		contentPane = new JPanel(){
-			public void paintComponent(Graphics g){
-				g.drawImage(image.background_ora.getImage(),0,0,null);
+
+		contentPane = new JPanel() {
+			public void paintComponent(Graphics g) {
+				g.drawImage(image.background_ora.getImage(), 0, 0, null);
 				setOpaque(false);
 				super.paintComponent(g);
 			}
@@ -35,7 +35,7 @@ public class RecipeDelivery extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		
+
 		JButton btn_back = new JButton();
 		btn_back.setFocusPainted(false);
 		btn_back.setContentAreaFilled(false);
@@ -47,17 +47,16 @@ public class RecipeDelivery extends JFrame {
 				dispose();
 			}
 		});
-		
-		
+
 		JLabel money_image = new JLabel(image.Money);
 		money_image.setHorizontalAlignment(SwingConstants.CENTER);
 		money_image.setBounds(12, 21, 202, 59);
 		contentPane.add(money_image);
-		
+
 		JLabel back_image = new JLabel(image.Back);
 		back_image.setBounds(12, 713, 50, 41);
 		contentPane.add(back_image);
-		
+
 		JTextField tf_money = new JTextField();
 		tf_money.setBackground(new Color(245, 245, 245));
 		tf_money.setHorizontalAlignment(SwingConstants.CENTER);
@@ -65,131 +64,172 @@ public class RecipeDelivery extends JFrame {
 		tf_money.setBounds(204, 21, 513, 54);
 		contentPane.add(tf_money);
 		tf_money.setColumns(10);
-		
+
 		JButton btn_중식 = new JButton(image.중식1);
 		btn_중식.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.chinahandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.chinahandler();
+
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
 			}
 		});
 		btn_중식.setBounds(62, 116, 290, 105);
 		contentPane.add(btn_중식);
-		
-		
+
 		JButton btn_치킨 = new JButton(image.치킨1);
 		btn_치킨.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.chickenhandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.chickenhandler();
+
+				}
+
+				catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
 			}
 		});
 		btn_치킨.setBounds(427, 116, 290, 105);
 		contentPane.add(btn_치킨);
-		
-		
+
 		JButton btn_일식 = new JButton(image.일식1);
 		btn_일식.addActionListener(new ActionListener() {
-				public void actionPerformed(ActionEvent arg0) {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
 					p.setPrice(new Integer(tf_money.getText()));
 					delivery.japanfoodhandler();
+
 				}
+
+				catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
+			}
 		});
 		btn_일식.setBounds(62, 259, 290, 105);
 		contentPane.add(btn_일식);
-		
+
 		JButton btn_한식_분식 = new JButton(image.분식1);
 		btn_한식_분식.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.koreanfoodhandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.koreanfoodhandler();
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
+
 			}
 		});
 		btn_한식_분식.setBounds(427, 259, 290, 105);
 		contentPane.add(btn_한식_분식);
-		
+
 		JButton btn_족발_보쌈 = new JButton(image.족발1);
 		btn_족발_보쌈.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.figsfeethandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.figsfeethandler();
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
+
 			}
 		});
 		btn_족발_보쌈.setBounds(427, 410, 290, 105);
 		contentPane.add(btn_족발_보쌈);
-		
+
 		JButton btn_피자 = new JButton(image.피자1);
 		btn_피자.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.pizzahandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.pizzahandler();
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
+
 			}
 		});
 		btn_피자.setBounds(427, 557, 290, 105);
 		contentPane.add(btn_피자);
-		
+
 		JButton btn_양식 = new JButton(image.양식1);
 		btn_양식.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.westernfoodhandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.westernfoodhandler();
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
 			}
 		});
 		btn_양식.setBounds(62, 410, 290, 105);
 		contentPane.add(btn_양식);
-		
+
 		JButton btn_찜_탕 = new JButton(image.찜1);
 		btn_찜_탕.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				p.setPrice(new Integer(tf_money.getText()));
-				delivery.steamhandler();
+				try {
+					p.setPrice(new Integer(tf_money.getText()));
+					delivery.steamhandler();
+				} catch (java.lang.NumberFormatException e) {
+					JOptionPane.showMessageDialog(null, "'가격'을 입력해 주세요");
+				}
+
 			}
 		});
 		btn_찜_탕.setBounds(62, 557, 290, 105);
 		contentPane.add(btn_찜_탕);
-		
+
 		JLabel lb_중식 = new JLabel("\uC911\uC2DD");
 		lb_중식.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_중식.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_중식.setBounds(166, 218, 78, 31);
 		contentPane.add(lb_중식);
-		
+
 		JLabel lb_치킨_찜닭 = new JLabel("\uCE58\uD0A8/\uCC1C\uB2ED");
 		lb_치킨_찜닭.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_치킨_찜닭.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_치킨_찜닭.setBounds(511, 218, 115, 31);
 		contentPane.add(lb_치킨_찜닭);
-		
+
 		JLabel lb_일식 = new JLabel("\uC77C\uC2DD");
 		lb_일식.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_일식.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_일식.setBounds(166, 361, 78, 31);
 		contentPane.add(lb_일식);
-		
+
 		JLabel lb_한식_분식 = new JLabel("\uD55C\uC2DD/\uBD84\uC2DD");
 		lb_한식_분식.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_한식_분식.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_한식_분식.setBounds(520, 361, 106, 31);
 		contentPane.add(lb_한식_분식);
-		
+
 		JLabel lb_양식 = new JLabel("\uC591\uC2DD");
 		lb_양식.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_양식.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_양식.setBounds(166, 511, 78, 31);
 		contentPane.add(lb_양식);
-		
+
 		JLabel lb_족발_보쌈 = new JLabel("\uC871\uBC1C/\uBCF4\uC308");
 		lb_족발_보쌈.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_족발_보쌈.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_족발_보쌈.setBounds(520, 511, 106, 31);
 		contentPane.add(lb_족발_보쌈);
-		
+
 		JLabel lb_찜_탕 = new JLabel("\uCC1C/\uD0D5");
 		lb_찜_탕.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_찜_탕.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
 		lb_찜_탕.setBounds(166, 662, 78, 31);
 		contentPane.add(lb_찜_탕);
-		
+
 		JLabel lb_피자_패스트푸드 = new JLabel("\uD53C\uC790/\uD328\uC2A4\uD2B8\uD478\uB4DC");
 		lb_피자_패스트푸드.setHorizontalAlignment(SwingConstants.CENTER);
 		lb_피자_패스트푸드.setFont(new Font("타이포_쌍문동 B", Font.PLAIN, 21));
